dd09787e7fa0744139bb453fba9da0a5061cbb44
rn123 <ingitfil@gmail.com>
2021-11-12 03:55:14 +0200

    lisatty testit projektiin Unicafe seka muokattu projektin junit 4.12 dependency pom.xml tiedostoon

---
+++ laskarit/viikko2/Unicafe/src/test/java/MaksukorttiTest.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MaksukorttiTest {
+
+    Maksukortti kortti;
+
+    @Before
+    public void setUp() {
+        kortti = new Maksukortti(10);
+    }
+
+    @Test
+    public void luotuKorttiOlemassa() {
+        assertTrue(kortti!=null);      
+    }
+}
